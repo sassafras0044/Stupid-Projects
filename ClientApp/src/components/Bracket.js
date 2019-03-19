3c41a50b981dd55ea41b4f8ede3816f00ade86ac
Taylor <tjordan@nextekdevelopment.com>
2019-03-19 16:39:58 -0500

    Left and Right Bracket changes

--- ClientApp/src/components/Bracket.js
+++ ClientApp/src/components/Bracket.js
@@ -1,120 +1,19 @@
 ﻿import React from 'react';
-import './bracket.css';
+import LeftBracket from './LeftBracket';
+import RightBracket from './RightBracket';
 
 class Bracket extends React.Component {
 
     render() {
         return (
-            <main id="tournament">
-                <ul className="round round-1">
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                </ul>
-                <ul className="round round-2">
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                </ul>
-                <ul className="round round-3">
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                </ul>
-                <ul className="round round-4">
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-                    <li className="game game-spacer">&nbsp;</li>
-                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                </ul>
-                <ul className="round round-5">
-                    <li className="spacer">&nbsp;</li>
-
-                    <li className="game game-top"><span className='ui button mini team'></span></li>
-
-                    <li className="spacer">&nbsp;</li>
-                </ul>
-            </main>
+            <div>
+                <div>
+                    <LeftBracket></LeftBracket>
+                </div>
+                <div>
+                    <RightBracket></RightBracket>
+                </div>
+            </div>
         );
     }
 }
---
+++ ClientApp/src/components/RightBracket.js
@@ -0,0 +1,220 @@
+﻿import React from 'react';
+import './rightBracket.css';
+
+class RightBracket extends React.Component {
+
+    render() {
+        return (
+            <main id="tournament">
+                <ul className="round round-5">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+                <ul className="round round-5">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+                <ul className="round round-4">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+                <ul className="round round-3">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+                <ul className="round round-2">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+                <ul className="round round-1">
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom "><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+
+                    <li className="game game-top"><span className='ui button mini team'></span></li>
+                    <li className="game game-spacer">&nbsp;</li>
+                    <li className="game game-bottom"><span className='ui button mini team'></span></li>
+
+                    <li className="spacer">&nbsp;</li>
+                </ul>
+            </main>
+        );
+    }
+}
+
+export default RightBracket;
